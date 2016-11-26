0320a33e0f71673c979185652e8f4bf29eb9a36d
jandolejs1999 <jandolejs1999@gmail.com>
2016-11-26 18:32:14 +0100

    Takže?

--- lesson-05/script.js
+++ lesson-05/script.js
@@ -1,7 +1,11 @@
 function hlaska() {
-	alert('1')
+	if ((document.getElementById('number').value) == '') {alert('CHYBA: Je nezbytné nejprve vyplnit text')}
+	else if ((document.getElementById('number').value.match(/^\d+$/))) {alert('Zvolili jste si číslo: ' + document.getElementById('number').value)}
+	else alert('CHYBA: zadali jste něco jiného, než číslo')
 }
 
 function spustit() {
 document.getElementById("main_button").onclick=hlaska
 }
+
+
