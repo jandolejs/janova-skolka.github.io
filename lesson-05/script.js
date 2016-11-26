03d7d9a04b80b19e62b34b045f43de329336e016
jandolejs1999 <jandolejs1999@gmail.com>
2016-11-26 19:05:41 +0100

    Hezkost +++

--- lesson-05/script.js
+++ lesson-05/script.js
@@ -1,18 +1,19 @@
-function hlaska() {
-
+function hlaska() 
+{
 	if ((document.getElementById('number').value) == '') 
-		{
-			alert('CHYBA: Je nezbytné nejprve vyplnit text')
-		}
+	{
+		alert('CHYBA: Je nezbytné nejprve vyplnit text');
+	}
 
 	else if ((document.getElementById('number').value.match(/^\d+$/))) 
-		{
-			alert('Zvolili jste si číslo: ' + document.getElementById('number').value)
-		}
+	{
+		alert('Zvolili jste si číslo: ' + document.getElementById('number').value);
+	}
 
-	else alert('CHYBA: zadali jste něco jiného, než číslo')
+	else alert('CHYBA: zadali jste něco jiného, než číslo');
 }
 
-function spustit() {
-	document.getElementById("main_button").onclick = hlaska
+function spustit() 
+{
+	document.getElementById("main_button").onclick = hlaska;
 }
