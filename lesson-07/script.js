cedde84feda19fcbd2b6ed8b7238c62dd227fd8b
jandolejs1999 <jandolejs1999@gmail.com>
2017-06-21 16:30:13 +0200

    :+1:

--- lesson-07/script.js
+++ lesson-07/script.js
@@ -1,7 +1,7 @@
 function hlaska() {
 	if ((document.getElementById("number").value) == "") {
 		alert("CHYBA: Je nezbytné nejprve vyplnit text");
-	} else if ((document.getElementById("number").value.match(/^\d+$/))) {
+	} else if ((document.getElementById("number").value.match(/^\d+[,.]?\d*$/))) {
 		alert("Zvolili jste si číslo: " + document.getElementById("number").value);
 	} else {
 		alert("CHYBA: zadali jste něco jiného, než číslo");
