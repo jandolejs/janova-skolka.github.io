323bee25a40953d2dfc90616367a92f2087ef734
Jan Dolejš <jandolejs1999@gmail.com>
2017-07-01 11:22:03 +0200

    Zkrácení z "document.getElementById" na "gid"

--- lesson-09/script.js
+++ lesson-09/script.js
@@ -1,22 +1,21 @@
 var yearformat;
 var heightformat;
 
-function spustit() {
-	document.getElementById("main_button").setAttribute("disabled", "disabled");
-	yearformat = document.getElementById("birth_year").getAttribute("data-format");
-	heightformat = document.getElementById("height").getAttribute("data-format");
-	document.getElementById("main_button").onclick = hlaska;
-	document.getElementById("birth_year").oninput = kontrola;
-	document.getElementById("height").oninput = kontrola;
+function spustit(){
+	gid("main_button").setAttribute("disabled", "disabled");
+	yearformat = gid("birth_year").getAttribute("data-format");
+	heightformat = gid("height").getAttribute("data-format");
+	gid("main_button").onclick = hlaska;
+	gid("birth_year").oninput = kontrola;
+	gid("height").oninput = kontrola;
 }
 
 function hlaska(){
-	if ((document.getElementById("birth_year").value.match(yearformat)) && (document.getElementById("height").value.match(heightformat))){
-	var jmeno = document.getElementById("name").value;
-	var datum = document.getElementById("birth_year").value;
-	var vyska = document.getElementById("height").value;
+	if ((gid("birth_year").value.match(yearformat)) && (gid("height").value.match(heightformat))){
+	var jmeno = gid("name").value;
+	var datum = gid("birth_year").value;
+	var vyska = gid("height").value;
 	alert("Zadali jste tyto údaje:" + "\nJméno: " + jmeno + "\nNarozen: " + datum + "\nVýška: " + vyska + " m");
-	// alert(yearformat + "\n" + heightformat);
 	}
 	else{
 		alert("CHYBA: Pole obsahují neplatnou hodnotu");
@@ -24,13 +23,16 @@ function hlaska(){
 }
 
 function kontrola(){
-	yearformat = document.getElementById("birth_year").getAttribute("data-format");
-	heightformat = document.getElementById("height").getAttribute("data-format");
-
-	if ((document.getElementById("birth_year").value.match(yearformat)) && (document.getElementById("height").value.match(heightformat))){
-		document.getElementById("main_button").removeAttribute("disabled");
+	yearformat = gid("birth_year").getAttribute("data-format");
+	heightformat = gid("height").getAttribute("data-format");
+	if ((gid("birth_year").value.match(yearformat)) && (gid("height").value.match(heightformat))){
+		gid("main_button").removeAttribute("disabled");
 	}
 	else {
-		document.getElementById("main_button").setAttribute("disabled", "disabled");
+		gid("main_button").setAttribute("disabled", "disabled");
 	}
-}	
+}
+
+function gid(id){
+	return document.getElementById(id);
+}
