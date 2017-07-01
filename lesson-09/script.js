e091961b341c9579ce396aa485e2448f0dd62c0f
Jan Dolejš <jandolejs1999@gmail.com>
2017-07-01 19:48:01 +0200

    ještě mi tam chybí to s tou mezerou

--- lesson-09/script.js
+++ lesson-09/script.js
@@ -1,5 +1,8 @@
 var yearformat;
 var heightformat;
+var pole1;
+var pole2;
+var pole3;
 
 function spustit(){
 	gid("main_button").setAttribute("disabled", "disabled");
@@ -8,6 +11,8 @@ function spustit(){
 	gid("main_button").onclick = hlaska;
 	gid("birth_year").oninput = kontrola;
 	gid("height").oninput = kontrola;
+	gid("name").oninput = kontrola;
+	kontrola();
 }
 
 function hlaska(){
@@ -25,14 +30,90 @@ function hlaska(){
 function kontrola(){
 	yearformat = gid("birth_year").getAttribute("data-format");
 	heightformat = gid("height").getAttribute("data-format");
-	if ((gid("birth_year").value.match(yearformat)) && (gid("height").value.match(heightformat))){
-		gid("main_button").removeAttribute("disabled");
-	}
-	else {
-		gid("main_button").setAttribute("disabled", "disabled");
-	}
+	nameformat = gid("height").getAttribute("data-format");
+
+
+//HERE XXXXX
+
+if(porovnat("name") == true && porovnat("birth_year") == true && porovnat("height") == true){
+	gid("main_button").removeAttribute("disabled");
+}
+else{
+	gid("main_button").setAttribute("disabled", "disabled");
+}
+
+//HEREXXXXX
+
+
 }
 
 function gid(id){
 	return document.getElementById(id);
+}
+
+
+function porovnat(jmenopole){
+	if(gid(jmenopole).value > ""){
+		if(gid(jmenopole).getAttribute("data-required")){
+			if(gid(jmenopole).getAttribute("data-required").match("yes")){
+				if(gid(jmenopole).getAttribute("data-format")){
+					if(gid(jmenopole).value.match(gid(jmenopole).getAttribute("data-format"))){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				else{
+					if(gid(jmenopole).getAttribute("data-format")){
+						if(gid(jmenopole).value.match(getAttribute("data-format"))){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					else{
+						return true;
+					}
+				}
+			}
+			else{
+				if(gid(jmenopole).getAttribute("data-format")){
+					if(gid(jmenopole).value.match(gid(jmenopole).getAttribute("data-format"))){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				else{
+					if(gid(jmenopole).getAttribute("data-format")){
+						if(gid(jmenopole).value.match(getAttribute("data-format"))){
+							return true;
+						}
+						else{
+							return false;
+						}
+					}
+					else{
+						return true;
+					}				
+				}
+			}
+		}
+	}
+	if(gid(jmenopole).value == ""){
+		if(gid(jmenopole).getAttribute("data-required")){
+			if(gid(jmenopole).getAttribute("data-required").match("yes")){
+				return false;
+			}
+			else{
+				return true;
+			}	
+		}
+		else{
+			return true;
+		}
+	}
 }
