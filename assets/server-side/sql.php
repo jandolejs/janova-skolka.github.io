b93751f0c56fca5bf7d40939079a370050f0e835
Jakub Bouček <pan@jakubboucek.cz>
2017-09-25 23:00:24 +0200

    Lesson 10

---
+++ assets/server-side/sql.php
@@ -0,0 +1,78 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/sql-credentials.php';
+
+if($_SERVER['HTTP_ORIGIN']) {
+	header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
+
+function getPost($name, $default = NULL) {
+	if(isset($_POST[$name])) {
+		return $_POST[$name];
+	}
+	else {
+		return $default;
+	}
+}
+
+$sql = getPost('query');
+
+if( !$sql ) {
+	errorResponse("No query sent");
+}
+
+$database = SqlCredentials::getConnection();
+
+try {
+	$query = $database->query($sql);
+	$rowsCount = $query->getRowCount();
+	$insertedId = $database->getInsertId();
+
+	$data = [];
+	try{
+		while(($row = $query->fetch()) !== FALSE) {
+			$data[] = iterator_to_array($row);
+		}
+	}
+	catch(\PDOException $e) {
+		if($e->getCode() == 'HY000') { // CRUD queries has not fetch ability
+			$data = FALSE;
+		}
+		else throw $e;
+	}
+
+	successResponse([
+		'results' => $data,
+		'rows' => $rowsCount,
+		'insertedId' => $insertedId,
+	]);
+}
+catch(\PDOException $e) {
+	errorResponse("Chybný SQL: " . $e->getMessage() . " (code: " . $e->getCode() . ")");
+}
+catch(\Exception $e) {
+	errorResponse("Neznámá chyba: " . $e->getMessage() . " (code: " . $e->getCode() . ")");
+}
+
+function successResponse($data) {
+	$response = [
+		'status' => TRUE,
+		'data' => $data,
+	];
+	sendResponse($response);
+	die();
+}
+
+function errorResponse($message) {
+	$response = [
+		'status' => FALSE,
+		'error' => $message,
+	];
+	sendResponse($response);
+	die();
+}
+
+function sendResponse($response) {
+	header('Content-Type: application/json');
+	echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+}
