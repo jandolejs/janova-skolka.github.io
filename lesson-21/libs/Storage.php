b4836c35b41be88f134f1826d5c2570bdd6e8457
Jan Dolejš <jandolejs1999@gmail.com>
2018-07-30 15:38:04 +0200

    přidána vlastní funkce getNewFileName

--- lesson-21/libs/Storage.php
+++ lesson-21/libs/Storage.php
@@ -8,19 +8,27 @@
         static function save($name, $formData)
         {
 
-            $name = Strings::webalize($name);
             $dataToWrite = json_encode($formData);
             $outputFolder = "output";
 
             do {
-                $fileName = $name."-".rand(1000,9999);
-                $outputPatch = "$outputFolder/$fileName.json";
+                $name = Storage::getNewFileName($name);
+                $outputPatch = "$outputFolder/$name.json";
             } while (file_exists($outputPatch));
 
-            $outputPatch = "$outputFolder/$fileName.json";
+            $outputPatch = "$outputFolder/$name.json";
             $outputFile = fopen($outputPatch, "wb");
 
             fwrite($outputFile, $dataToWrite);
             fclose($outputFile);
+        }
+
+        static function getNewFileName($name)
+        {
+
+            $name = Strings::webalize($name);
+            $name = $name."-".rand(1000,9999);
+
+            return $name;
         }
     }
