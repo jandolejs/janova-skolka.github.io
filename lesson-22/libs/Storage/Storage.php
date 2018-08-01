4eec7986e7589e45d761096c5f4df342d40ae2b6
Jan Dolejš <jandolejs1999@gmail.com>
2018-08-01 11:17:12 +0200

    už to jde

--- lesson-22/libs/Storage/Storage.php
+++ lesson-22/libs/Storage/Storage.php
@@ -10,17 +10,17 @@ class Storage
 
     private $path;
 
-    function __construct($_path)
+    function __construct($path)
     {
-        $path = $_path;
+        $this->path = $path;
     }
 
-    static function save($name, $formData)
+    function save($name, $formData)
     {
         $formData['date'] = date(DATE_ATOM);
         $dataToWrite = json_encode($formData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
-        $filePath = self::getNewFilePath($name);
+        $filePath = $this->getNewFilePath($name);
 
         $isFileSaved = @file_put_contents($filePath, $dataToWrite);
 
@@ -30,19 +30,19 @@ class Storage
     }
 
 
-    static function getNewFilePath($name)
+    function getNewFilePath($name)
     {
-        $outputFolder = $path;
+        $outputFolder = $this->path;
 
         $date = date('Y-m-d-H-i-s');
-        $name = self::sanitizeName($name);
+        $name = $this->sanitizeName($name);
         $random = Random::generate(4);
 
         return "$outputFolder/$date-$name-$random.json";
     }
 
 
-    static function sanitizeName($name)
+    function sanitizeName($name)
     {
         $name = Strings::webalize($name);
         $name = Strings::truncate($name, '30', '');
