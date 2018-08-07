7a5198450f724e816ac3dcd459428ea338eff1c4
Jan Dolejš <jandolejs1999@gmail.com>
2018-08-07 09:18:07 +0200

    fix chyby formátování

--- libs/Validator/Validate.php
+++ libs/Validator/Validate.php
@@ -36,7 +36,7 @@ class Validate
 
     static function password($value)
     {
-        if (strlen($value) < 8) {
+        if (strlen(utf8_decode($value)) < 8) {
             throw new ValidateException('Heslo musí obsahovat alespoň 8 znaků');
         }
         return true;
