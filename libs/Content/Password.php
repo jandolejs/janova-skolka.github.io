b71b874cac5da2d77833bfbf768fbe12b981e286
Jan Dolejš <jandolejs1999@gmail.com>
2018-08-07 12:34:09 +0200

    lepší heslování

--- libs/Content/Password.php
+++ libs/Content/Password.php
@@ -6,8 +6,22 @@ use App\Validator\Validate;
 
 class Password extends DataObject
 {
+
+    private $hashed;
+
+    public function __construct($content)
+    {
+        parent::__construct($content);
+        $this->hashed = password_hash($content, PASSWORD_BCRYPT);
+    }
+
     protected function validate($content)
     {
         Validate::required($content) && Validate::password($content);
+    }
+
+    public function getHash()
+    {
+        return $this->hashed;
     }
 }
